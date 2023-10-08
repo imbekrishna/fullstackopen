5602372b2b9fde4ec66c5d036b0d8e398f32daf8
imbekrishna <imbekrishna@gmail.com>
2023-10-08 09:45:43 +0530

    completed 1-d

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,53 @@
 import { useState } from "react";
 
-const Display = ({counter}) => {
+const Display = ({ counter }) => {
   return <div>{counter}</div>;
 };
 
-const Button = ({handleClick, text}) => {
+const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
-  const decreaseByOne = () => setCounter(counter - 1);
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
+
+  const [allClicks, setAllClicks] = useState([]);
+
+  const handleLeftClick = () => {
+    setAllClicks(allClicks.concat("L"));
+    const updatedLeft = clicks.left + 1;
+    setClicks({
+      ...clicks,
+      left: updatedLeft,
+    });
+  };
+  const handleRightClick = () => {
+    setAllClicks(allClicks.concat("R"));
+    const updatedRight = clicks.right + 1;
+    setClicks({
+      ...clicks,
+      right: updatedRight,
+    });
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button text="minus" handleClick={decreaseByOne} />
-      <Button text="reset" handleClick={setToZero} />
-      <Button text="plus" handleClick={increaseByOne} />
+      <Display counter={clicks.left} />
+      <Button text="left" handleClick={handleLeftClick} />
+      <Display counter={clicks.right} />
+      <Button text="right" handleClick={handleRightClick} />
+      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };

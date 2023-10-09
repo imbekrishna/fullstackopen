bac450bf22d9412eddc4a88d82b841b24df2251c
imbekrishna <imbekrishna@gmail.com>
2023-10-09 14:44:42 +0530

    updated app to work with backend

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -9,15 +9,10 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-
     axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
       setNotes(response.data);
     });
   }, []);
-
-  console.log(`render ${notes.length} notes`);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -27,12 +22,24 @@ const App = () => {
       id: notes.length + 1,
     };
 
-    setNotes([...notes, noteObject]);
-    setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      setNotes([...notes, response.data]);
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    });
   };
 
   const notesToShow = showAll
@@ -44,7 +51,11 @@ const App = () => {
       <h1>Notes</h1>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>

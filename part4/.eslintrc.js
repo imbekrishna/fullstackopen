43af232328e9be83bb374d50e47dfbef86d1dfc2
imbekrishna <imbekrishna@gmail.com>
2023-10-12 00:47:44 +0530

    add jest and write tests for testing

--- part4/.eslintrc.js
+++ part4/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    jest: true,
   },
   extends: 'eslint:recommended',
   overrides: [
@@ -20,14 +21,14 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
-    'indent': ['error', 2],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'eqeqeq': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 0
+    'no-console': 0,
   },
 };

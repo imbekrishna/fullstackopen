352a0a81ff1d08832405d0ca976d0dbf20922ade
imbekrishna <imbekrishna@gmail.com>
2023-10-13 01:57:10 +0530

    add users model | move notes request to folder

--- part4/models/note.js
+++ part4/models/note.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
   content: { type: String, minLength: 5, required: true },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 noteSchema.set('toJSON', {

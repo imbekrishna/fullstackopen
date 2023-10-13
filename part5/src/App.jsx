d91633a3cbed4047d746c10140333165e31c07ee
imbekrishna <imbekrishna@gmail.com>
2023-10-13 19:16:05 +0530

    add part 5

---
+++ part5/src/App.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+import Notification from "./components/Notification";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+  const [erroMessage, setErroMessage] = useState(null);
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    noteService.create(noteObject).then((note) => {
+      setNotes([...notes, note]);
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((note) => {
+        setNotes(notes.map((n) => (n.id !== id ? n : note)));
+      })
+      .catch(() => {
+        setErroMessage(
+          `The note '${note.content}' was already removed from server.`
+        );
+        setTimeout(() => {
+          setErroMessage(null);
+        }, 5000);
+        setNotes(notes.filter((note) => note.id !== id));
+      });
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={erroMessage} />
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+    </div>
+  );
+};
+
+export default App;

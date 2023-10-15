476702b496e70333bb14bdc1c73a1ac4d574278e
imbekrishna <imbekrishna@gmail.com>
2023-10-16 01:47:26 +0530

    add react-redux | refactor state management

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import noteReducer from './reducers/noteReducer.js';
+
+const store = createStore(noteReducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);

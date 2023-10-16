1bfe794761180b09a23d8f8aad6c6373ccbe04d3
imbekrishna <imbekrishna@gmail.com>
2023-10-16 19:09:00 +0530

    add note service backend

--- part6/src/components/NewNote.jsx
+++ part6/src/components/NewNote.jsx
@@ -1,19 +1,21 @@
 import { useDispatch } from 'react-redux';
 import { createNote } from '../reducers/noteReducer';
+import noteService from '../services/notes';
 
 const NewNote = () => {
   const dispatch = useDispatch();
 
-  const addNote = (event) => {
+  const addNote = async (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = '';
-    dispatch(createNote(content));
+    const newNote = await noteService.createNew(content);
+    dispatch(createNote(newNote));
   };
 
   return (
-    <form className='noteForm' onSubmit={addNote}>
-      <input type="text" name="note" placeholder='A new note...' />
+    <form className="noteForm" onSubmit={addNote}>
+      <input type="text" name="note" placeholder="A new note..." />
       <button type="submit">add</button>
     </form>
   );

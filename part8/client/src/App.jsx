ec704a39f6c21e36ec3059e60d9e92d2531cce59
imbekrishna <imbekrishna@gmail.com>
2023-10-20 12:13:10 +0530

    add login form

--- part8/client/src/App.jsx
+++ part8/client/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css';
+
+import Notification from './components/Notification';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-import Notification from './components/Notification';
-
-import { useQuery } from '@apollo/client';
-import { ALL_PERSONS } from './queries';
-import { useState } from 'react';
+import LoginForm from './components/LoginForm';
 import PhoneForm from './components/PhoneForm';
+
+import { useApolloClient, useQuery } from '@apollo/client';
+import { useState } from 'react';
+import { ALL_PERSONS } from './queries';
 
 function App() {
   const [errorMessages, setErrorMessages] = useState(null);
+  const [token, setToken] = useState(null);
+
   const result = useQuery(ALL_PERSONS);
+  const client = useApolloClient();
 
   const notify = (message) => {
     setErrorMessages(message);
@@ -19,12 +24,30 @@ function App() {
     }, 10000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem('phone-number-users-token');
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <Notification errorMessage={errorMessages} />
+        <h2>Login</h2>
+        <LoginForm setError={notify} setToken={setToken} />
+      </div>
+    );
+  }
+
   if (result.loading) {
     return <>loading ...</>;
   }
+
   return (
     <>
       <Notification errorMessage={errorMessages} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
---
+++ part8/client/src/components/LoginForm.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { LOGIN } from '../queries';
+import { useMutation } from '@apollo/client';
+
+const LoginForm = ({ setToken, setError }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message);
+    },
+  });
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value;
+      setToken(token);
+      localStorage.setItem('phone-number-users-token', token);
+    }
+  }, [result.data]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    login({ variables: { username, password } });
+
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button id="login-button" type="submit">
+          login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
